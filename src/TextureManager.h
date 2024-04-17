b6dafc70fc550ed43374932af71c82d35f580482
Overbrighten <overbrightenart@outlook.com>
2024-04-17 22:57:42 +0800

    texture defines

--- src/TextureManager.h
+++ src/TextureManager.h
@@ -1,14 +1,40 @@
 #pragma once
 
 #include <driver_types.h>
+#include <string>
+#include <vector>
 
 #include "MathCommon.h"
 
 namespace YumeRT
 {
+	struct ImageFile 
+	{
+		std::string file_name;
+		__host__ inline ImageFile() {}
+		__host__ inline ImageFile(const std::string &name) : file_name(name) {}
+	};
+
+	struct ImageTile
+	{
+		void *device_data;
+		void *host_data;
+
+		__device__ __host__ inline ImageTile() : device_data(nullptr), host_data(nullptr) {}
+	};
+
 	// should be single instance, the same as Scene
 	struct ImageTileCache
 	{
-		int x;
+		ImageTile *image_tiles;
+		__device__ __host__ inline ImageTileCache() : image_tiles(nullptr) {}
+	};
+
+	struct ImageTextureManager 
+	{
+		ImageTileCache image_tile_cache;
+		std::vector<ImageFile> image_texture_files;
+		std::vector<ImageTile> image_texture_tiles;
+		__host__ inline ImageTextureManager() : image_tile_cache(), image_texture_files(0), image_texture_tiles(0) {}
 	};
 };
